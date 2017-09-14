f55bc3f012d1f2b11110085403cebcf53f2f02c3
ChadPunk <chade2540@gmail.com>
2017-09-14 16:34:18 -0400

    First Commit

---
+++ resources/js/notify.js
@@ -0,0 +1,10 @@
+function notify() {
+    var number = 0;
+    if(number == 1){
+  document. getElementById('show_notify'). style. display ='block';
+  }
+else if(number == 0){
+  document. getElementById('show_notify'). style. display ='none';
+  }
+
+}
